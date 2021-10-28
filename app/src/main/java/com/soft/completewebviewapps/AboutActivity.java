3511ef73e9f91fe65e050b2143e5bdb3d51a5720
Mofizur Rahman <mofizur853@gmail.com>
2021-10-28 14:44:35 +0600

    First Commit On This Project

---
+++ app/src/main/java/com/soft/completewebviewapps/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.soft.completewebviewapps;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        //set title actionBar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("DMCA");
+    }
+}
